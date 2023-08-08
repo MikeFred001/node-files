0035aeff5243f2ccbdcbfb293cd868bae318bbfd
michael frederickson <mpfplus@gmail.com>
2023-08-08 13:29:39 -0600

    Working on step 2 of exercise

---
+++ step2.js
@@ -0,0 +1,22 @@
+const fsP = require('fs/promises');
+
+const argv = process.argv;
+console.log(argv);
+
+if (argv.length > 3) {
+  process.exit(1);
+}
+
+
+async function cat(path) {
+  try {
+    let contents = await fsP.readFile(`${path}`, "utf8");
+    console.log(contents);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+
+cat(argv[2]);
